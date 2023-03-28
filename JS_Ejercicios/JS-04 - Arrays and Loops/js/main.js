bececc5aaa49db297deb6ac815b89d8248ea2faa
gaby <gabriela.torres.044@gmail.com>
2023-03-27 20:56:21 -0600

    Se concluyeron todos los ejercicios de tarea

--- JS_Ejercicios/JS-04 - Arrays and Loops/js/main.js
+++ JS_Ejercicios/JS-04 - Arrays and Loops/js/main.js
@@ -22,10 +22,52 @@ for (let a = 1; a <= 5; a++) {
     for(let b = 1; b<= a; b++) {
         pattern += "* ";
     }
-
     console.log (pattern);
 }
 
+console.log("EXERCISE 3");              
+console.log("++++++ 1 ++++++");
+//– Repeatedly print the value of the variable xValue, decreasing it by 0.5 each time,
+// as long as xValue remains positive.
+// console.log("Elige un valor para la función x escribiendo: x(valor);");      //Para el caso de querer introducir el valor de x
+// function x (xValue){               //Para el caso de querer introducir el valor de x
+let xValue = 4;                      //Para el caso de querer que X inicie en un valor establecido previamente
+while (xValue >= 0) {
+    console.log (xValue);
+    xValue = xValue - 0.5;
+}
+// ]
+
+console.log("++++++ 2 ++++++");     //Print all the odd numbers between 1 - 100.
+let odd = 1;
+while (odd <= 100) {
+    if (odd % 2 != 0) {             //Si el residuo de odd entre 2 es diferente de 0
+        console.log (odd);
+    }
+    odd ++;
+}
+
+console.log("++++++ 3 ++++++");                     //Write a method with a while loop to print 1 through n in square brackets. 
+console.log("Elige un valor para la función n escribiendo: n(valor);");
+function n (nValue) {
+    let brackets =1;
+    while ( brackets <= nValue) {
+    console.log ("[" + brackets + "]" );
+    brackets ++;
+    }
+}
+
+console.log("++++++ 4 ++++++");     //Write a method with a while loop that computes the sum of first n positive integers:
+console.log("Elige un valor para la función sum escribiendo: sum(valor);");
+function sum (max) {
+let b = 0;
+let num = 1;
+while (num <= max) {
+    b = b + num;
+    // console.log (b);
+    num++;
+} return ("La suma es " + b);
+}
 
 
 // https://github.com/gaby-tm/JS_Ejercicios/tree/master/JS-04%20-%20Arrays%20and%20Loops
